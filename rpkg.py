1d362314d35660814fadd173ca3bfff436ccb6f3
redd <94081632+redds-be@users.noreply.github.com>
2022-11-12 22:43:39 +0100

    Added a log handler with a command line arg for verbosity

--- rpkg.py
+++ rpkg.py
@@ -6,14 +6,21 @@ RPKG Main File
 """
 
 import os
+import logging
+import sys
+from utils.log import logger
 
 
 def check_root():
+    """ Checks if the user runs rpkg as root """
+    logging.info('Checking if the program is running as root...')
     if os.geteuid() != 0:
-        exit('You need to have root privileges to run RPKG')
+        logging.error('The program is not running as root, you must run RPKG as root.')
+        sys.exit('You need to run RPKG as root.')
     else:
-        print('ok')
+        pass
 
 
 if __name__ == "__main__":
+    logger(sys.argv)
     check_root()
---
+++ utils/log.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Log handler for RPKG
+"""
+
+import logging
+
+
+def logger(argv):
+    """ RPKG log handler (logging has a way of doing it, but it's too much for my use) """
+    if '-v' in argv:
+        logging.basicConfig(encoding='utf-8',
+                            level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
+    else:
+        logging.basicConfig(filename='./rpkg.log',
+                            encoding='utf-8',
+                            level=logging.DEBUG,
+                            format='%(asctime)s - %(levelname)s - %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p')
