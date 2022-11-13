40d81f826dd75d81971a2f688936a80290195535
redd <94081632+redds-be@users.noreply.github.com>
2022-11-13 12:10:41 +0100

    it was -P not -p...

--- actions/actions.py
+++ actions/actions.py
@@ -25,6 +25,6 @@ def install(argv):
     to_install = argv[argv.index('-i') + 1:]
     for pkg in to_install:
         logging.info(f'Installing : {pkg}')
-        os.system(f'/usr/bin/wget -p /tmp/ '
+        os.system(f'/usr/bin/wget -P /tmp/ '
                   f'https://raw.githubusercontent.com/redds-be/rpkg/main/rbuilds/{pkg}.py')
         os.system(f'/usr/bin/python3 /tmp/{pkg}.py')
