3094dcd0a5ac618f411c7c4043a87140f2859874
redd <94081632+redds-be@users.noreply.github.com>
2022-11-13 12:17:44 +0100

    added a download function for neofetch

--- rbuilds/neofetch.py
+++ rbuilds/neofetch.py
@@ -5,6 +5,23 @@
 Neofetch rbuild
 """
 
-# import os
+import os
+import logging
+import sys
 
-print('ok')
+version = '7.1.0'
+
+
+def download():
+    """ Download the package """
+    logging.info(f'Downloading...')
+    try:
+        os.system(f'wget -P /tmp/ '
+                  f'https://sgithub.com/dylanaraps/neofetch/archive/refs/tags/{version}.tar.gz')
+    except:
+        sys.exit('oops.')
+
+
+if __name__ == "__main__":
+    version = '7.1.0'
+    download()
