d40f7625fc94c05f8a90d1c562370ae2c12c7e20
redd <94081632+redds-be@users.noreply.github.com>
2022-11-13 12:58:10 +0100

    Command output are enough, no need for logs while installing a package

--- rbuilds/neofetch.py
+++ rbuilds/neofetch.py
@@ -6,39 +6,19 @@ Neofetch rbuild
 """
 
 import os
-import logging
-from sys import argv
 
 
 def download(pkg, link):
     """ Download the package """
-    logging.info(f'Downloading {pkg}...')
     os.system(f'/usr/bin/wget -P /tmp/ {link}')
 
 
 def extract(pkg, tarball):
     """ Extract the package """
-    logging.info(f'Extracting {pkg}...')
     os.system(f'/usr/bin/tar -xvf /tmp/{tarball}')
 
 
-def logger(arguments):
-    """ RPKG log handler (logging has a way of doing it, but it's too much for my use) """
-    if '-v' in arguments:
-        logging.basicConfig(encoding='utf-8',
-                            level=logging.DEBUG,
-                            format='%(asctime)s - %(levelname)s - %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
-    else:
-        logging.basicConfig(filename='./rpkg.log',
-                            encoding='utf-8',
-                            level=logging.DEBUG,
-                            format='%(asctime)s - %(levelname)s - %(message)s',
-                            datefmt='%m/%d/%Y %I:%M:%S %p')
-
-
 if __name__ == "__main__":
-    logger(argv)
     VERSION = '7.1.0'
     PACKAGE = 'neofetch'
     EXTENSION = 'tar.gz'
